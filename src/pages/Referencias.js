18fc13d0fa6c34f02eb9e03649f6e45953874101
Cristian Henao <cristian.henao@siesa.com>
2021-03-09 23:00:17 -0500

    mod

--- src/pages/Referencias.js
+++ src/pages/Referencias.js
@@ -9,7 +9,7 @@ function Referencias() {
         <>
           <Header />
           <div className="container"> 
-              <h1>Referencias Bibliograficas y Webgraficas.</h1>
+              <h1>Referencias Webgraficas.</h1>
               <ListGroup variant="flush">
                 {data.map((values) => (
                     <ListGroup.Item key={values.idReferencia} >{values.referencia}</ListGroup.Item>    
